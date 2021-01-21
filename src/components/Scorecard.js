bd5809255fedc79fdd1bd4e2026c0e1903a1f1fd
zfinnan <63361320+zfinnan@users.noreply.github.com>
2021-01-21 12:17:44 -0500

    fix

--- src/components/Scorecard.js
+++ src/components/Scorecard.js
@@ -2,12 +2,9 @@ import React from "react";
 // import { Switch } from 'react-router-dom'
 
 const Scorecard = () => {
-  const players = [
-    {position: "forwad", name: "lebron", team: "lakers"},
-    {position: "guard", name: "russell westbrook", team: "rockets"},
-    {position: "", name: "", team: ""},
-    {position: "", name: "", team: ""},
-  ]
+  
+
+  
   return (
     <div>
       <h1>New Round</h1>

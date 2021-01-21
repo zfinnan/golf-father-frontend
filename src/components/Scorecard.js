5a2992b1ada90def9ae6a4e94523d25fb61848e7
zfinnan <63361320+zfinnan@users.noreply.github.com>
2021-01-21 14:09:31 -0500

    add data to post function

--- src/components/Scorecard.js
+++ src/components/Scorecard.js
@@ -291,6 +291,94 @@ const Scorecard = (props) => {
   const handlePlayerFour = (e) => {
     setPlayerFour(e.target.value);
   }
+  const [redirect, setRedirect] = useState(false);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("test");
+    const data = {
+      day: roundDay,
+      month: roundMonth,
+      year: roundYear,
+      course: course,
+      frontNineTotalYardage: frontNineYardage,
+      backNineTotalYardage: backNineYardage,
+      totalYardage: totalYardage,
+      holeOneYardage: holeOneYardage,
+      holeTwoYardage: holeTwoYardage,
+      holeThreeYardage: holeThreeYardage,
+      holeFourYardage: holeFourYardage,
+      holeFiveYardage: holeFiveYardage,
+      holeSixYardage: holeSixYardage,
+      holeSevenYardage: holeSevenYardage,
+      holeEightYardage: holeEightYardage,
+      holeNineYardage: holeNineYardage,
+      holeTenYardage: holeTenYardage,
+      holeElevenYardage: holeElevenYardage,
+      holeTwelveYardage: holeTwelveYardage,
+      holeThirteenYardage: holeThirteenYardage,
+      holeFourteenYardage: holeFourteenYardage,
+      holeFifteenYardage: holeFifteenYardage,
+      holeSixteenYardage: holeSixteenYardage,
+      holeSeventeenYardage: holeSeventeenYardage,
+      holeEighteenYardage: holeEighteenYardage,
+      frontNinePar: frontNinePar,
+      backNinePar: backNinePar,
+      totalPar: totalPar,
+      holeOnePar: holeOnePar,
+      holeTwoPar: holeTwoPar,
+      holeThreePar: holeThreePar,
+      holeFourPar: holeFourPar,
+      holeFivePar: holeFivePar,
+      holeSixPar: holeSixPar,
+      holeSevenPar: holeSevenPar,
+      holeEightPar: holeEightPar,
+      holeNinePar: holeNinePar,
+      holeTenPar: holeTenPar,
+      holeElevenPar: holeElevenPar,
+      holeTwelvePar: holeTwelvePar,
+      holeThirteenPar: holeThirteenPar,
+      holeFourteenPar: holeFourteenPar,
+      holeFifteenPar: holeFifteenPar,
+      holeSixteenPar: holeSixteenPar,
+      holeSeventeenPar: holeSeventeenPar,
+      holeEighteenPar: holeEighteenPar,
+      frontNineScore: frontNineScore,
+      backNineScore: backNineScore,
+      totalScore: totalScore,
+      holeOneScore: holeOneScore,
+      holeTwoScore: holeTwoScore,
+      holeThreeScore: holeThreeScore,
+      holeFourScore: holeFourScore,
+      holeFiveScore: holeFiveScore,
+      holeSixScore: holeSixScore,
+      holeSevenScore: holeSevenScore,
+      holeEightScore: holeEightScore,
+      holeNineScore: holeNineScore,
+      holeTenScore: holeTenScore,
+      holeElevenScore: holeElevenScore,
+      holeTwelveScore: holeTwelveScore,
+      holeThirteenScore: holeThirteenScore,
+      holeFourteenScore: holeFourteenScore,
+      holeFifteenScore: holeFifteenScore,
+      holeSixteenScore: holeSixteenScore,
+      holeSeventeenScore: holeSeventeenScore,
+      holeEighteenScore: holeEighteenScore,
+      playerOne: playerOne,
+      playerTwo: playerTwo,
+      playerThree: playerThree,
+      playerFour: playerFour
+    };
+    // console.log(data)
+    axios
+      .post(`${REACT_APP_SERVER_URL}/api/expenses/new`, {
+        data,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setRedirect(true);
+      });
+  };
 
   return (
     <div>
